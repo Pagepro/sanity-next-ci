a79a88b7d787a84032b0f23dbf418f3b0f52449e
Jakub Dakowicz <jakub.dakowicz@pagepro.co>
2024-10-22 14:21:08 +0200

    change name of file

--- src/app/schemaTypes/index.ts
+++ src/app/schemaTypes/index.ts
@@ -1,9 +1,17 @@
-import {cta} from './cta'
-import {cta2} from './cta2'
-import {ctaBlock} from './ctaBlock'
-import {heroBlock} from './heroBlock'
-import {imageCarousel} from './imageCarousel'
-import {page} from './page'
-import {textBlock} from './textBlock'
+import { cta } from "./cta";
+import { cta2 } from "./cta2";
+import { ctaBlock } from "./ctaBlock";
+import { heroBlock } from "./heroBlock";
+import { imageCarousel } from "./imageCarousel";
+import { page } from "./pageType";
+import { textBlock } from "./textBlock";
 
-export const schemaTypes = [cta, cta2, ctaBlock, heroBlock, imageCarousel, page, textBlock]
+export const schemaTypes = [
+  cta,
+  cta2,
+  ctaBlock,
+  heroBlock,
+  imageCarousel,
+  page,
+  textBlock,
+];
